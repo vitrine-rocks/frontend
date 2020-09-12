cb17e294998944c2f3f88ca80302c1c4cee45ccc
Marco Rojo <mrmarcondes@gmail.com>
2020-09-12 15:33:04 -0300

    start adding some colors

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,27 @@
 <template>
   <v-app light>
     <v-container>
+      <v-app-bar
+        dense
+        light
+        flat
+        color="primary"
+      >
+        <v-app-bar-nav-icon />
+
+        <v-img
+          :src="require('~/assets/logo.png')"
+          class="ml-2"
+          max-width="40"
+          max-height="20"
+        />
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+      </v-app-bar>
       <nuxt />
     </v-container>
   </v-app>
